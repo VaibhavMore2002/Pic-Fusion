ecadb470254df7fc68641924767fa213ff984d80
VaibhavMore2002 <morevaibhav545@gmail.com>
2024-04-18 02:07:28 +0530

    Database Errors Resolved

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,5 +1,5 @@
-import { error } from "console";
 import mongoose, { Mongoose } from "mongoose";
+
 const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
@@ -8,21 +8,27 @@ interface MongooseConnection {
 }
 
 let cached: MongooseConnection = (global as any).mongoose;
+
 if (!cached) {
   cached = (global as any).mongoose = {
     conn: null,
     promise: null,
   };
 }
+
 export const connectToDatabase = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
-  if (!process.env.MONGODB_URL) throw new Error("Missing MONGODB_URL");
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+
   cached.promise =
     cached.promise ||
-    mongoose.connect(process.env.MONGODB_URL, {
+    mongoose.connect(MONGODB_URL, {
       dbName: "picfusion",
       bufferCommands: false,
     });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 };
